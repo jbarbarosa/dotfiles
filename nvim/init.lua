fa6ace15babc1f4d787c57fc89429a640b71e201
Joao P B <joaopb@MacBook-Pro-de-Paulo.local>
2022-06-17 16:15:52 -0300

    add telescope, treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,5 +4,21 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
 
 Plug 'morhetz/gruvbox'
 
+Plug('nvim-telescope/telescope-fzf-native.nvim', {['do'] = vim.fn['cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build']})
+
+Plug('nvim-telescope/telescope-fzf-native.nvim', {['do'] = vim.fn['make']})
+
+Plug 'nvim-lua/plenary.nvim'
+
+Plug 'nvim-telescope/telescope.nvim'
+
+Plug('nvim-treesitter/nvim-treesitter', {['do'] = vim.fn[':TSUpdate']})
+
+Plug 'williamboman/nvim-lsp-installer'
+
+Plug 'neovim/nvim-lspconfig'
+
 vim.call('plug#end')
 
+require("nvim-lsp-installer").setup {}
+require("telescope").setup {}
